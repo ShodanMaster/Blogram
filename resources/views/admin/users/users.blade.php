32a948574346dfa3c186eafb250496f97bbc7fbf
ShodanMaster <akshaykrishnan922@gmail.com>
2025-02-12 18:22:15 +0530

    Yajra for Users

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -2,49 +2,6 @@
 @section('content')
     <h1>Users</h1>
 
-    {{-- <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Registered Through</th>
-                <th scope="col">Restricted</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($users as $user)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td><a href="{{route('admin.userprofile', encrypt($user->id))}}">{{ $user->name }}</a></td>
-                    <td>{{ $user->email }}</td>
-                    <td>
-                        @if($user->google_id)
-                            Google
-                        @else
-                            Registration
-                        @endif
-                    </td>
-                    <td>
-                        <div class="user-card">
-                            <span id="userStatus{{ $user->id }}" class="badge {{ $user->ban ? 'bg-danger' : 'bg-success' }}">
-                                {{ $user->ban ? 'Banned' : 'Active' }}
-                            </span>
-
-                            <button type="button" class="btn btn-{{ $user->ban ? 'success' : 'danger' }}" id="banButton" value="{{ $user->id }}">
-                                {{ $user->ban ? 'Unban' : 'Ban' }}
-                            </button>
-                        </div>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="5" class="text-center">No users found.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table> --}}
-
     <table class="table table-striped table-hover" id="usersTable">
         <thead>
             <tr>
